539437e8dc068d46416630a15ac8694b0e67a7df
Bart Massey <bart.massey@gmail.com>
2021-04-27 12:38:53 -0700

    cleaned up top example

--- top.rs
+++ top.rs
@@ -6,7 +6,7 @@ struct Top<'a, T> {
     // Maximum number of elements to contain.
     count: usize,
     // Element ordering function.
-    order: fn(&&T, &&T) -> Ordering,
+    order: fn(&T, &T) -> Ordering,
     // Elements contained, by reference.
     elems: Vec<&'a T>,
 }
@@ -21,12 +21,10 @@ impl<T: fmt::Debug> fmt::Debug for Top<'_, T> {
     }
 }
 
-
 // Can parameterize a function to make it able to take a specified type of data.
-
-fn make_top<'a, T>(count: usize, order: fn(&&T, &&T) -> Ordering, vals: &'a [T]) -> Top<'a, T> {
+fn make_top<T>(count: usize, order: fn(&T, &T) -> Ordering, vals: &[T]) -> Top<'_, T> {
     let mut elems: Vec<&T> = vals.iter().collect();
-    elems.sort_by(order);
+    elems.sort_by(|&v1, &v2| order(v1, v2));
     if count < elems.len() {
         elems.truncate(count);
     }
@@ -37,16 +35,19 @@ fn make_top<'a, T>(count: usize, order: fn(&&T, &&T) -> Ordering, vals: &'a [T])
 impl<'a, T> Top<'a, T> {
     fn update(&mut self, val: &'a T) {
         self.elems.push(val);
-        self.elems.sort_by(self.order);
-        while self.elems.len() > self.count {
+        let order = self.order;
+        self.elems.sort_by(|&v1, &v2| order(v1, v2));
+        if self.elems.len() > self.count {
             let _ = self.elems.pop();
         }
+        assert!(self.elems.len() <= self.count);
     }
 }
 
 fn main() {
     let vals = [9, 7, 3, 1, 4, 6, 8, 2, 5];
     let mut top = make_top(3, |x, y| x.cmp(y), &vals);
+    println!("{:?}", top);
     top.update(&0);
     println!("{:?}", top);
 }
