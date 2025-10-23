280e92b9a83bd1a31512c187d1526c84bab53e52
Bart Massey <bart.massey@gmail.com>
2025-10-22 21:00:54 -0700

    cleaned up and improved

--- bno-salad.rs
+++ bno-salad.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 // From Blandy & Orendorff's *Programming Rust*
 
 pub trait Vegetable {
@@ -8,7 +10,13 @@ pub trait Vegetable {
 pub struct Lettuce;
 impl Vegetable for Lettuce {
     fn desc(&self) -> &'static str {
-        self.desc()
+        self.lettuce_desc()
+    }
+}
+
+impl Lettuce {
+    fn lettuce_desc(&self) -> &'static str {
+        "lettuce"
     }
 }
 
@@ -16,63 +24,76 @@ impl Vegetable for Lettuce {
 pub struct Tomato;
 impl Vegetable for Tomato {
     fn desc(&self) -> &'static str {
-        self.desc()
+        self.tomato_desc()
+    }
+}
+
+impl Tomato {
+    fn tomato_desc(&self) -> &'static str {
+        "tomato"
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Cucumber;
+
+impl Vegetable for Cucumber {
+    fn desc(&self) -> &'static str {
+        self.cucumber_desc()
+    }
+}
+
+impl Cucumber {
+    fn cucumber_desc(&self) -> &'static str {
+        "cucumber"
     }
 }
 
 pub struct GenericSalad<T: Vegetable> {
-    veggies: Vec<T>,
+    bowl: Vec<T>,
 }
 
 impl<T: Vegetable> GenericSalad<T> {
     fn new() -> Self {
         GenericSalad {
-            veggies: Vec::new(),
+            bowl: Vec::new(),
         }
     }
 
     fn add(&mut self, v: T) {
-        self.veggies.push(v);
+        self.bowl.push(v);
     }
 
     pub fn display(&self) {
-        for v in &self.veggies {
+        for v in &self.bowl {
             println!("{}", v.desc());
         }
     }
 }
 
-pub struct TraitSalad {
-    veggies: Vec<Box<dyn Vegetable>>,
+pub struct TraitSalad<'a> {
+    bowl: Vec<Box<dyn Vegetable + 'a>>,
 }
 
-impl TraitSalad {
+impl<'a> TraitSalad<'a> {
     fn new() -> Self {
         TraitSalad {
-            veggies: Vec::new(),
+            bowl: Vec::new(),
         }
     }
 
     fn add(&mut self, v: Box<dyn Vegetable>) {
-        self.veggies.push(v);
+        self.bowl.push(v);
+    }
+
+    fn add_direct<T: Vegetable + 'a>(&mut self, v: T) {
+        self.bowl.push(Box::new(v));
     }
 
     pub fn display(&self) {
-        for v in &self.veggies {
+        for v in &self.bowl {
             println!("{}", v.desc());
         }
-    }
-}
-
-impl Lettuce {
-    fn desc(&self) -> &'static str {
-        "lettuce"
-    }
-}
-
-impl Tomato {
-    fn desc(&self) -> &'static str {
-        "tomato"
     }
 }
 
@@ -84,22 +105,22 @@ enum SaladVegetable {
 use SaladVegetable::*;
 
 pub struct EnumSalad {
-    veggies: Vec<SaladVegetable>,
+    bowl: Vec<SaladVegetable>,
 }
 
 impl EnumSalad {
     fn new() -> Self {
         EnumSalad {
-            veggies: Vec::new(),
+            bowl: Vec::new(),
         }
     }
 
     fn add(&mut self, v: SaladVegetable) {
-        self.veggies.push(v);
+        self.bowl.push(v);
     }
 
     pub fn display(&self) {
-        for v in &self.veggies {
+        for v in &self.bowl {
             match v {
                 SaladLettuce => println!("lettuce"),
                 SaladTomato => println!("tomato"),
@@ -109,7 +130,7 @@ impl EnumSalad {
     }
 }
 
-pub fn make_me_a_generic_lettuce_salad<T: Vegetable + Clone>(veggie: T) ->
+pub fn make_me_a_generic_salad<T: Vegetable + Clone>(veggie: T) ->
     GenericSalad<T>
 {
     let mut generic_salad = GenericSalad::new();
@@ -119,18 +140,37 @@ pub fn make_me_a_generic_lettuce_salad<T: Vegetable + Clone>(veggie: T) ->
     generic_salad
 }
 
-pub fn make_me_a_trait_salad() -> TraitSalad {
+pub fn make_me_a_trait_salad() -> TraitSalad<'static> {
     let mut trait_salad = TraitSalad::new();
-    trait_salad.add(Box::new(Lettuce));
-    trait_salad.add(Box::new(Tomato));
+    trait_salad.add_direct(Lettuce);
+    trait_salad.add_direct(Tomato);
     trait_salad
 }
 
-pub fn make_me_a_enum_salad() -> EnumSalad {
+pub fn make_me_an_enum_salad() -> EnumSalad {
     let mut enum_salad = EnumSalad::new();
     enum_salad.add(SaladLettuce);
     enum_salad.add(SaladTomato);
     enum_salad.add(SaladTomato);
     enum_salad.add(SaladCucumber);
     enum_salad
+}
+
+fn main() {
+    let mut salad: GenericSalad<Lettuce> = make_me_a_generic_salad(Lettuce);
+    salad.display();
+    // salad.add(Tomato);
+    // salad.display();
+    
+    println!();
+
+    let salad = make_me_an_enum_salad();
+    salad.display();
+
+    println!();
+
+    let mut salad = make_me_a_trait_salad();
+    salad.display();
+    salad.add_direct(Cucumber);
+    salad.display();
 }
--- square-macro.rs
+++ square-macro.rs
@@ -1,11 +1,12 @@
 macro_rules! square {
-    ($s:expr) => {{
+    ($s:expr) => {
         ($s) * ($s)
-    }};
+    };
 }
 
 fn main() {
     let mut n = 3_u8;
     println!("3**2={}", square!(n));
     println!("4**2={}", square!({n += 1; n}));
+    println!("{}", n);
 }
